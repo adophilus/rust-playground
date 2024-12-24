6db6c4304e271953ac4c3df72e60b88fd72f263f
Adophilus <uchenna19of@gmail.com>
2024-12-24 23:18:14 +0100

    worked on static serve

--- solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/backend/src/app.ts
+++ solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/backend/src/app.ts
@@ -1,7 +1,12 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { router as nftRouter } from "./routes/nft";
+import { serveStatic } from "@hono/node-server/serve-static";
 
-export const app = new Hono().use(logger()).route("/nft", nftRouter);
+export const app = new Hono()
+  .use(logger())
+  .route("/nft", nftRouter)
+  .use(serveStatic({ root: "./public" }))
+  .use(serveStatic({ path: "./public/index.html" }));
 
 export type App = typeof app;
