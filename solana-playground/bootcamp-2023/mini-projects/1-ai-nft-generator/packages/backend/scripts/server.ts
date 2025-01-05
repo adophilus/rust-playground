3c35a89a2c0374614e8c6c5f2673f6bf5e8ffbf4
Adophilus <uchenna19of@gmail.com>
2025-01-05 20:37:14 +0100

    updated the server

--- solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/backend/scripts/server.ts
+++ solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/backend/scripts/server.ts
@@ -1,23 +1,23 @@
 import { env } from "@/env";
-import { handle } from "hono/vercel";
+// import { handle } from "hono/vercel";
 import { app } from "@/app";
 import { serve } from "@hono/node-server";
 
-const handler = handle(app);
+// const handler = handle(app);
 
-export const GET = handler;
-export const DELETE = handler;
-export const OPTIONS = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const POST = handler;
+// export const GET = handler;
+// export const DELETE = handler;
+// export const OPTIONS = handler;
+// export const PATCH = handler;
+// export const PUT = handler;
+// export const POST = handler;
 
-if (env.NODE_ENV === "development")
-  serve(
-    {
-      fetch: app.fetch,
-      port: env.PORT,
-    },
-    ({ port, address }) =>
-      console.log(`Server is running on http://${address}:${port}`),
-  );
+// if (env.NODE_ENV === "development")
+serve(
+  {
+    fetch: app.fetch,
+    port: env.PORT,
+  },
+  ({ port, address }) =>
+    console.log(`Server is running on http://${address}:${port}`),
+);
