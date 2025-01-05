47f46bc6ecbe1cd11cc998a38d5c8b009bafc76e
Adophilus <uchenna19of@gmail.com>
2025-01-05 21:27:11 +0100

    updated the env vars

--- solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/frontend/src/env.tsx
+++ solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/frontend/src/env.tsx
@@ -3,7 +3,9 @@ import { z } from "zod";
 export const env = z
   .object({
     REOWN_PROJECT_ID: z.string(),
+    BACKEND_SERVER_URL: z.string().url(),
   })
   .parse({
     REOWN_PROJECT_ID: import.meta.env.VITE_REOWN_PROJECT_ID,
+    BACKEND_SERVER_URL: import.meta.env.VITE_BACKEND_SERVER_URL,
   });
--- solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/frontend/src/lib/http.tsx
+++ solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/frontend/src/lib/http.tsx
@@ -1,4 +1,5 @@
 import { hc } from "hono/client";
 import type { App } from "@nft-ai-generator/backend";
+import { env } from "@nft-ai-generator/frontend/env";
 
-export const backendClient = hc<App>(window.location.origin);
+export const backendClient = hc<App>(env.BACKEND_SERVER_URL);
