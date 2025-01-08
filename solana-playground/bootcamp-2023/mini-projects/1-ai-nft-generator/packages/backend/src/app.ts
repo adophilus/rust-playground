05db7fc12babd074f7b2f8a02d92e6872ab19ac7
Adophilus <uchenna19of@gmail.com>
2025-01-08 23:18:53 +0100

    updated the app

--- solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/backend/src/app.ts
+++ solana-playground/bootcamp-2023/mini-projects/1-ai-nft-generator/packages/backend/src/app.ts
@@ -2,10 +2,12 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { router as nftRouter } from "./routes/nft";
 import { serveStatic } from "@hono/node-server/serve-static";
+import { cors } from "hono/cors";
 
 const apiRouter = new Hono().route("/", nftRouter);
 
 export const app = new Hono()
+  .use(cors())
   .use(logger())
   .route("/api", apiRouter)
   .use(serveStatic({ root: "./public" }))
